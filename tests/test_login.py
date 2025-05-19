486f0a5b60e295201a3ad7664e605a27ee6bb20f
amnotbob <salmanpppqh@gmail.com>
2025-05-19 10:49:52 +0530

    Update test_login.py with Selenium script

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,9 @@
-import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 @pytest.fixture
 def driver():
